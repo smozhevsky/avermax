2b066a485f2951acca56ec0e0a0da10d6177e9eb
smozhevsky <smozhevsky@gmail.com>
2020-12-21 11:12:52 +0300

    fix the menu on screens <520px

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let menuButton = document.querySelector(".menu-button");
 let menu = document.querySelector(".header-inner");
 
 menuButton.addEventListener("click", function() {
-  menuButton.classList.toggle("menu-button-active");
-  menu.classList.toggle("header-inner-active");
+  menuButton.classList.toggle("active");
+  menu.classList.toggle("active");
 });
