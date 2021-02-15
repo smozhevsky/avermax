e5e714903447f299d51dc2aa84d9c1ebf2133abc
smozhevsky <smozhevsky@gmail.com>
2021-02-15 21:36:54 +0300

    add smooth scroll to anchors

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 let menuButton = document.querySelector(".menu-button");
 let menu = document.querySelector(".header-inner");
 
-menuButton.addEventListener("click", function() {
+menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("menu-button-active");
   menu.classList.toggle("header-inner-active");
 });
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
